2c1e6fd0fc91ee4a712f6eb273f1127caecca633
Ernesto <distritomx@gmail.com>
2021-02-01 20:15:45 -0800

    master

---
+++ build/precache-manifest.fb13b63b9418a073deab1b6c8efa6474.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4372a8a58252a01ffa5dfd1e5cde48d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bdbc81937195de4db647",
+    "url": "./static/css/main.7939ab5a.chunk.css"
+  },
+  {
+    "revision": "1161d1f58da4b61f61f3",
+    "url": "./static/js/2.35a29b2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.35a29b2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdbc81937195de4db647",
+    "url": "./static/js/main.fcb96693.chunk.js"
+  },
+  {
+    "revision": "15735b1bde69f25e90ee",
+    "url": "./static/js/runtime-main.0fcf83b3.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "./static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "./static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "./static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "29a2277ca1de4116a2c67631eb439879",
+    "url": "./static/media/logo_elsultan.29a2277c.png"
+  }
+]);
